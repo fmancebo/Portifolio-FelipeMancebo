43799d71b4d298dfc513be8ede62ce687a130804
Felipe <f.92xavier1@gmail.com>
2023-12-07 22:47:37 -0300

    resposividade atualizacao

--- JAVASCRIPT/scripts.js
+++ JAVASCRIPT/scripts.js
@@ -1,3 +1,19 @@
+const menuhamburguer = document.querySelector('.menu-hamburguer');
+const nav = document.querySelector('.navbar');
+const links = document.querySelectorAll('.navbar-links li a');
+
+// ativa e desativa menu
+menuhamburguer.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
+
+links.forEach(item => {
+    item.addEventListener('click', () => {
+      nav.classList.toggle('active');  
+    })
+})
+// END
+
 //CABEÇALHO
 var header = document.getElementById('header');             //para qualquer funçao no JS primeiro definimos a variavel
 
@@ -14,7 +30,7 @@ window.addEventListener('scroll', () => {                   //essa funçao defin
 //CARROCEL PREJETOS
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 2,
-    spaceBetween: 5,
+    spaceBetween: 30,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -26,5 +42,26 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 //END CARROCEL PROJETOS
+
+//FUNÇAO PARA SLIDE CONFORME LARGURA
+
+function handleWidth(){
+    let getWidth = window.innerWidth || document.documentElement.clientWidth;
+    slideShow = 2;
+
+    if(getWidth < 700){
+        slideShow = 1
+    }
+    // Atualiza a configuração do Swiper
+    swiper.params.slidesPerView = slideShow;
+    swiper.update();
+}
+//END FUNCCION
+
+// Chama a função handleWidth imediatamente quando a página é carregada
+handleWidth();
+
+// Adiciona um ouvinte de evento 'resize' ao objeto window
+window.addEventListener('resize', handleWidth);
 
 
